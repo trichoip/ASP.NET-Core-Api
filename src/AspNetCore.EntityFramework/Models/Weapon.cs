f9bf329c63585a1f1d753a4bab1362cdb5ea6889
trichoip <107303342+trichoip@users.noreply.github.com>
2023-09-21 17:36:20 +0700

    Update Weapon.cs

--- src/AspNetCore.EntityFramework/Models/Weapon.cs
+++ src/AspNetCore.EntityFramework/Models/Weapon.cs
@@ -8,6 +8,11 @@ namespace AspNetCore.EntityFramework.Models
         public string Name { get; set; }
 
         // lưu ý: child phải có properties đúng với cấu trúc ClassFatherId hoặc PropertiesFatherId
+        // int CharacterId -> khi migration thì nó tự thêm OnDelete(DeleteBehavior.Cascade)
+        // int? CharacterId ->  khi migration thì nó không có thêm OnDelete(DeleteBehavior.Cascade)
+        // nếu lúc đầu là int? thì khi migration thì nó không có thêm OnDelete(DeleteBehavior.Cascade)
+        // nếu lúc sau sửa lại là int bỏ ? thì khi migration thì nó update OnDelete(DeleteBehavior.Cascade)
+        // nêu lúc đầu là int thì nó thêm OnDelete(DeleteBehavior.Cascade) và lúc thêm ? thì nó xóa OnDelete(DeleteBehavior.Cascade)
         public int CharacterId { get; set; }
         [JsonIgnore]
         public virtual Character Nemo { get; set; }
